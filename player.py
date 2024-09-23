5d648ee72c2f52608db375c2edaa5905b3662958
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2024-09-23 18:30:35 +1000

    Create player.py
    The player object is used to track each player, allowing humans to select their moves and with frameworks in place to allow an AI to select its move.

---
+++ player.py
@@ -0,0 +1,44 @@
+from board import Board
+
+class Player:
+    player_white: bool
+    type: str
+
+    def __init__(self, player_white, type):
+        self.player_white = player_white
+        self.type = type
+
+    def get_player(self):
+        return player_white
+
+    def get_move_selection(self, board):
+        match self.type:
+            case "manual":
+                return self.manual_move(board, self.player_white)
+
+    def manual_move(self, board, player_white):
+        selected_coords = ()
+        available_moves = set()
+        piece_selection_ongoing = True
+        while piece_selection_ongoing:
+            x = int(input("x coordinate: "))
+            y = int(input("y coordinate: "))
+            # if the move is available
+            if (x,y) in available_moves:
+                # move the piece
+                board.move_piece(selected_coords[0], selected_coords[1], x, y)
+                # exit piece selection
+                piece_selection_ongoing = False
+            # if the piece at the selected coordinate is owned by the player
+            elif board.get_board()[y][x].player_white == player_white:
+                # select the piece
+                selected_coords = (x,y)
+                selected_piece = board.get_board()[y][x]
+                selected_piece.find_available_moves(board, x, y)
+                available_moves = selected_piece.get_available_moves()
+                print(available_moves)
+            # illegal choice
+            else:
+                print("Illegal.")
+        print()
+        return board
