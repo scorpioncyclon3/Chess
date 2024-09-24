3225fdbe5145f5bf0505930a2f2e4c020deba1cd
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2024-09-24 11:17:52 +1000

    Create knight.py

---
+++ Piece_Objects/knight.py
@@ -0,0 +1,21 @@
+from Piece_Objects.limited_movement_piece import Limited_Movement_Piece
+
+"""
+Moves in an L shape (2 spaces one direction, one sidways).
+"""
+
+class Knight(Limited_Movement_Piece):
+    # piece attributes
+    player_white: bool
+    value: int
+    available_moves: set[tuple[int, int]]
+    # limited movement piece attributes
+    direction = tuple[tuple[int, int]]
+
+    def __init__(self, player_white):
+        Limited_Movement_Piece.__init__(
+            self,
+            player_white,
+            value=3,
+            directions=((1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1,-2))
+        )
