bb2bef3987d809695e25da48e6d5a1a544dedb19
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2024-09-25 21:18:21 +1000

    Update main.py
    Added Check alert.

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ player_1_turn = True
 game_running = True
 while game_running:
     board.print_state()
+    # one side in check
+    white_king_in_check, black_king_in_check = board.check_for_check()
+    if white_king_in_check:
+        print("White king now in check.")
+    if black_king_in_check:
+        print("Black king now in check.")
+
     if player_1_turn:
         print("White turn.")
         player_1.get_move_selection(board)
