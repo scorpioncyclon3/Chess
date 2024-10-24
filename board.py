ca5583feccc1998ce560b07a2c48173913583eba
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2024-10-24 12:15:25 +1100

    Update board.py
    Changed the system to track remaining value so it is automatically calculated when pieces are added. 
    Removed an obsolete debugging statement.

--- board.py
+++ board.py
@@ -18,6 +18,9 @@ class Board:
             for j in range(0,8):
                 self.board[-1].append(None)
 
+        # tracks the total value on the board
+        self.total_white_value, self.total_black_value = 0, 0
+
         # fill board
         for (player_colour, y) in ((True, 7), (False, 0)):
             self.add_piece(King(player_white=player_colour), 4, y)
@@ -32,8 +35,6 @@ class Board:
             for x in range(0,8):
                 pass#self.add_piece(Pawn(player_white=player_colour), x, y)
         
-        # tracks how much value has been lost
-        self.total_white_value, self.total_black_value = 39, 39
 
     def get_board(self):
         return self.board
@@ -50,16 +51,14 @@ class Board:
                 # adds spaces between characters
                 row_str += " "
 
-                # for debugging
-                #print(str(type(self.board[y][x])))
-
-                
                 # empty space
                 if str(type(self.board[y][x])) == "<class 'NoneType'>":
                     row_str += "--"
                 else:
                     # adds the piece colour
-                    row_str += ("W" if self.board[y][x].player_white else "B")
+                    row_str += (
+                        "W" if self.board[y][x].player_white else "B"
+                    )
 
                     # adds a different character for each piece type
                     match str(type(self.board[y][x])):
@@ -85,20 +84,35 @@ class Board:
         print("")
 
     def add_piece(self, piece:object, x:int, y:int):
+        # adds the piece value to the total if it isn't a king
+        if str(type(piece)) != "<class 'Piece_Objects.king.King'>":
+            if piece.player_white:
+                self.total_white_value += piece.value
+            else:
+                self.total_black_value += piece.value
+        # adds the piece
         self.board[y][x] = piece
 
-    def move_piece(self, old_x:int, old_y:int, new_x:int, new_y:int, real:bool):
+    def move_piece(
+        self, old_x:int, old_y:int, new_x:int, new_y:int, real:bool
+    ):
         # if taking a piece
         if self.board[new_y][new_x] != None:
             # adds the value of it t the total
             if self.board[new_y][new_x].get_player():
-                self.total_white_value -= self.board[new_y][new_x].get_value()
+                self.total_white_value -= (
+                    self.board[new_y][new_x].get_value()
+                )
                 if real:
-                    print("Total white value remaining ", self.total_white_value)
+                    print("Total white value remaining ",
+                        self.total_white_value)
             else:
-                self.total_black_value -= self.board[new_y][new_x].get_value()
+                self.total_black_value -= (
+                    self.board[new_y][new_x].get_value()
+                )
                 if real:
-                    print("Total black value remaining ", self.total_black_value)
+                    print("Total black value remaining ",
+                        self.total_black_value)
 
         # moves the piece
         self.board[new_y][new_x] = self.board[old_y][old_x]
@@ -106,7 +120,9 @@ class Board:
         self.board[old_y][old_x] = None
 
         # castling
-        if str(type(self.get_board()[new_y][new_x])) == "<class 'Piece_Objects.king.King'>":
+        if str(type(self.get_board()[new_y][new_x])) == (
+            "<class 'Piece_Objects.king.King'>"
+        ):
             self.board[new_y][new_x].prevent_castling()
             # castle left
             if old_x - new_x == 2:
@@ -116,10 +132,14 @@ class Board:
             elif old_x - new_x == -2:
                 self.board[new_y][5] = self.board[new_y][7]
                 self.board[new_y][7] = None
-        elif str(type(self.get_board()[new_y][new_x])) == "<class 'Piece_Objects.rook.Rook'>":
+        elif str(type(self.get_board()[new_y][new_x])) == (
+            "<class 'Piece_Objects.rook.Rook'>"
+        ):
             self.board[new_y][new_x].prevent_castling()
         # pawn double movement
-        elif str(type(self.get_board()[new_y][new_x])) == "<class 'Piece_Objects.pawn.Pawn'>":
+        elif str(type(self.get_board()[new_y][new_x])) == (
+            "<class 'Piece_Objects.pawn.Pawn'>"
+        ):
             self.board[new_y][new_x].prevent_double_move()
         self.evaluate_state(real)
 
@@ -130,23 +150,35 @@ class Board:
         #loops through all positions
         for y in range(0,8):
             for x in range(0,8):
-                # if the space is not empty, add its available moves to all_available_moves
+                # if the space is not empty, add its available moves to
+                # all_available_moves
                 if str(type(self.get_board()[y][x])) != "<class 'NoneType'>":
                     # updates the piece's available_moves set
                     self.get_board()[y][x].find_available_moves(self, x, y)
                     if self.get_board()[y][x].player_white:
-                        # adds the items from the piece's available_moves set to the all_available_moves_white set
-                        all_available_moves_white.update(self.get_board()[y][x].get_available_moves())
+                        # adds the items from the piece's available_moves set
+                        # to the all_available_moves_white set
+                        all_available_moves_white.update(
+                            self.get_board()[y][x].get_available_moves()
+                        )
                         # if the piece is a king, track its position
-                        if str(type(self.get_board()[y][x])) == "<class 'Piece_Objects.king.King'>":
+                        if str(type(self.get_board()[y][x])) == (
+                            "<class 'Piece_Objects.king.King'>"
+                        ):
                             white_king_pos = (x,y)
                     else:
-                        # adds the items from the piece's available_moves set to the all_available_moves_black set
-                        all_available_moves_black.update(self.get_board()[y][x].get_available_moves())
+                        # adds the items from the piece's available_moves set
+                        # to the all_available_moves_black set
+                        all_available_moves_black.update(
+                            self.get_board()[y][x].get_available_moves()
+                        )
                         # if the piece is a king, track its position
-                        if str(type(self.get_board()[y][x])) == "<class 'Piece_Objects.king.King'>":
+                        if str(type(self.get_board()[y][x])) == (
+                            "<class 'Piece_Objects.king.King'>"
+                        ):
                             black_king_pos = (x,y)
-        # finds out whether either kings can be taken by a piece of the opposite colour
+        # finds out whether either kings can be taken by a piece of the
+        # opposite colour
         white_king_in_check, black_king_in_check = False, False
         if white_king_pos in all_available_moves_black:
             white_king_in_check = True
@@ -163,7 +195,10 @@ class Board:
         # converts ratio from opposing_piece_value : piece_value
         # to 1 : adjusted_piece_value, where a high adjusted_piece_value
         # is favourable
-        white_adjusted_piece_value = self.total_black_value / self.total_white_value
-        black_adjusted_piece_value = self.total_white_value / self.total_black_value
+        white_adjusted_piece_value = (
+            self.total_black_value / self.total_white_value)
+        black_adjusted_piece_value = (
+            self.total_white_value / self.total_black_value)
         if real:
-            print(white_adjusted_piece_value, black_adjusted_piece_value)
+            print("White adjusted piece value ", white_adjusted_piece_value)
+            print("Black adjusted piece value ", black_adjusted_piece_value)
