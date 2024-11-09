10a85fa6a20326e6d62c6d7e65738f00f0dd8bbc
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2024-11-09 14:22:57 +1100

    Update player.py
    Now determines whether each move causes checkmate. Moved remove_illegal_options() to the Board class (now "remove_illegal_moves_from_set").

--- player.py
+++ player.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from board import Board
 
 class Player:
@@ -13,31 +12,16 @@ class Player:
         return self.player_white
 
     def get_move_selection(self, board):
+        # checkmate
+        if board.check_for_checkmate(self.get_player()):
+            print("checkmate")
+            return True
+        # move selection
         match self.type:
             case "manual":
-                return self.manual_move(board, self.player_white)
-
-    def remove_illegal_options(self, board, available_moves, x, y):
-        # removes illegal moves
-        to_remove = set()
-        for move in available_moves:
-            print(move)
-            # copies the board
-            board_copy = deepcopy(board)
-            # simulates the move in the copy
-            board_copy.move_piece(
-                old_x=x, old_y=y, new_x=move[0], new_y=move[1], real=False
-            )
-            # checks whether it moves self into check
-            white_king_in_check, black_king_in_check = board_copy.check_for_check()
-            # if it moves self into check
-            if (self.get_player() and white_king_in_check) or (not self.get_player() and black_king_in_check):
-                # tracks the illegality of the move
-                to_remove.add((move[0],move[1]))
-        # removes the illegal moves
-        for move in to_remove:
-            available_moves.remove(move)
-        return available_moves
+                self.manual_move(board, self.player_white)
+        # not a checkmate
+        return False
 
     def manual_move(self, board, player_white):
         selected_coords = ()
@@ -74,7 +58,8 @@ class Player:
                 selected_piece = board.get_board()[y][x]
                 selected_piece.find_available_moves(board, x, y)
                 available_moves = selected_piece.get_available_moves()
-                available_moves = self.remove_illegal_options(board, available_moves, x, y)
+                available_moves = board.remove_illegal_moves_from_set(
+                    available_moves, self.get_player(), x, y)
 
             # illegal choice
             else:
