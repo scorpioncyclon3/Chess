a998ae6216db1056ef515033d187e2985b12b121
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2025-01-06 15:27:16 +1100

    Update main.py
    Added Minimax AI player option.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from board import Board
 from Player_Objects.player import Player
 from Player_Objects.manual_player import Manual_Player
+from Player_Objects.minimax_ai_player import Minimax_AI_Player
 
 board = Board()
 player_1 = Manual_Player(player_white=True)
 player_2 = Manual_Player(player_white=False)
+#player_1 = Minimax_AI_Player(player_white=True, max_recursion_depth=3)
+#player_2 = Minimax_AI_Player(player_white=False, max_recursion_depth=3)
 player_1_turn = True
 
 game_running = True
