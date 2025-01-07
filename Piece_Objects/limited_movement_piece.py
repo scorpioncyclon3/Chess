b6f8f94cae55cae57a9e100ba46543abd2edeb4a
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2025-01-07 18:18:45 +1100

    Update limited_movement_piece.py
    Added directional move finding with find_available_moves_in_direction() function.

--- Piece_Objects/limited_movement_piece.py
+++ Piece_Objects/limited_movement_piece.py
@@ -21,15 +21,21 @@ class Limited_Movement_Piece(Piece):
     def find_available_moves(self, board, x, y):
         self.available_moves = set()
         for direction in self.get_directions():
-            checking_x = x + direction[0]
-            checking_y = y + direction[1]
-            # if inside the board
-            if 0 <= checking_x <= 7 and 0 <= checking_y <= 7:
-                # piece in position being checked
-                if board.get_board()[checking_y][checking_x] != None:
-                    # piece of opposite team
-                    if board.get_board()[checking_y][checking_x].get_player() != self.get_player():
-                        self.available_moves.add((checking_x, checking_y))
-                # checking empty position
-                else:
+            self.find_available_moves_in_direction(board, x, y, direction)
+
+    def find_available_moves_in_direction(self, board, x, y, direction):
+        checking_x = x + direction[0]
+        checking_y = y + direction[1]
+        # if inside the board
+        if 0 <= checking_x <= 7 and 0 <= checking_y <= 7:
+            # piece in position being checked
+            if board.get_board()[checking_y][checking_x] is not None:
+                # piece of opposite team
+                if (
+                    board.get_board()[checking_y][checking_x].get_player()
+                    != self.get_player()
+                ):
                     self.available_moves.add((checking_x, checking_y))
+            # checking empty position
+            else:
+                self.available_moves.add((checking_x, checking_y))
