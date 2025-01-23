4128d0036de569e601d56cb1c28950ac912432a1
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2025-01-23 16:19:49 +1100

    Update main.py
    Moved checkmate checking into main.py after each move.

--- main.py
+++ main.py
@@ -6,29 +6,34 @@ from Player_Objects.minimax_ai_player import Minimax_AI_Player
 board = Board()
 player_1 = Manual_Player(player_white=True)
 player_2 = Manual_Player(player_white=False)
-#player_1 = Minimax_AI_Player(player_white=True, max_recursion_depth=3)
-#player_2 = Minimax_AI_Player(player_white=False, max_recursion_depth=3)
+#player_1 = Minimax_AI_Player(player_white=True, max_recursion_depth=2)
+#player_2 = Minimax_AI_Player(player_white=False, max_recursion_depth=2)
 player_1_turn = True
 
 game_running = True
 while game_running:
     board.print_state()
-    # one side in check
+
+    #  checkmate notification
     white_king_in_check, black_king_in_check = board.check_for_check()
     if white_king_in_check:
         print("White king in check.")
     if black_king_in_check:
         print("Black king in check.")
 
+    # player turns
     if player_1_turn:
         print("White turn.")
-        board, checkmate = player_1.move(board)
+        board = player_1.move(board)
     else:
         print("Black turn.")
-        board, checkmate = player_2.move(board)
+        board = player_2.move(board)
+
+    # switches turn
     player_1_turn = not player_1_turn
-    print(checkmate)
+    
+    checkmate = board.check_for_checkmate(player_1_turn)
     if checkmate:
         print("Checkmate.")
-        print(("White" if player_1_turn else "Black") + " wins.")
+        print(("Black" if player_1_turn else "White") + " wins.")
         game_running = False
