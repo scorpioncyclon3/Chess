c817d6d0061061b588cec3029290713b87be823e
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2025-03-22 13:45:13 +1100

    Update play.py
    Now records the time taken per turn and the turn counter.

--- play.py
+++ play.py
@@ -1,12 +1,23 @@
+import time
+
 from board import Board
 from Player_Objects.player import Player
 from Player_Objects.manual_player import Manual_Player
 from Player_Objects.minimax_ai_player import Minimax_AI_Player
 
-def play_game(player_1: Player, player_2: Player, data_collection: bool = False):
+def play_game(
+    player_1: Player,
+    player_2: Player,
+    data_collection: bool = False,
+    trial_name: str = ""
+):
+    # setup
     player_1_turn = True
     board = Board()
+    turn = 0
+    total_time = 0.0
     game_running = True
+
     while game_running:
         board.print_state()
 
@@ -18,15 +29,27 @@ def play_game(player_1: Player, player_2: Player, data_collection: bool = False)
             print("Black king in check.")
 
         # player turns
+        start_time = time.perf_counter()
         if player_1_turn:
             print("White turn.")
             board = player_1.move(board)
         else:
             print("Black turn.")
             board = player_2.move(board)
+        end_time = time.perf_counter()
+        turn_time = end_time - start_time
+        print(f"Time taken for turn {turn}: {turn_time}")
+        total_time += turn_time
+
+        # records the turn if it is a real trial
+        if data_collection:
+            pass
 
         # switches turn
         player_1_turn = not player_1_turn
+
+        # increments the turn counter
+        turn += 1
         
         checkmate = board.check_for_checkmate(player_1_turn)
         if checkmate:
