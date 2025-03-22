cf8a124e9f2d22865af17f5d15144eeb9aa56a8a
scorpioncyclon3 <144417251+scorpioncyclon3@users.noreply.github.com>
2025-03-22 12:33:16 +1100

    Update main.py
    Created frameworks for automated data collection. The play_game function now takes the optional boolean parameter data_collection.

--- main.py
+++ main.py
@@ -3,9 +3,57 @@ from Player_Objects.manual_player import Manual_Player
 from Player_Objects.minimax_ai_player import Minimax_AI_Player
 from play_game import play_game
 
-player_1 = Manual_Player(player_white=True)
-player_2 = Manual_Player(player_white=False)
-player_1 = Minimax_AI_Player(player_white=True, max_recursion_depth=2)
-player_2 = Minimax_AI_Player(player_white=False, max_recursion_depth=2)
+single_game = False
+#single_game = True
 
-play_game(player_1, player_2)
+# plays a single game for testing purposes or for fun
+if single_game:
+    p1, p2 = 0, 0
+    match p1:
+        case 0:
+            player_1 = Manual_Player(player_white=True)
+        case 1:
+            player_1 = Minimax_AI_Player(player_white=True, max_recursion_depth=2)
+        case _:
+            player_1 = Manual_Player(player_white=True)
+    match p2:
+        case 0:
+            player_2 = Manual_Player(player_white=False)
+        case 1:
+            player_2 = Minimax_AI_Player(player_white=False, max_recursion_depth=2)
+        case _:
+            player_1 = Manual_Player(player_white=True)
+    play_game(player_1, player_2)
+
+# automated data collection
+else:
+    for depth in range(1,4):
+        player_1 = Minimax_AI_Player(
+            player_white=True, 
+            max_recursion_depth=depth,
+        )
+        player_2 = Minimax_AI_Player(
+            player_white=False,
+            max_recursion_depth=depth, 
+        )
+        play_game(
+            player_1,
+            player_2,
+            data_collection=True
+        )
+
+        """
+        player_1 = Alpha_Beta_AI_Player(
+            player_white=True,
+            max_recursion_depth=depth,
+        )
+        player_2 = Alpha_Beta_AI_Player(
+            player_white=False,
+            max_recursion_depth=depth,
+        )
+        play_game(
+            player_1,
+            player_2,
+            data_collection=True
+        )
+        """
